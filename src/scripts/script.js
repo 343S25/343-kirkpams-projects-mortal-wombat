c7b0c87efe35d4c72c87582326b7da8646f18c2a
Will Upright <w.e.upright@gmail.com>
2025-04-16 17:16:17 -0400

    Start of Specific Inputs

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,3 +6,26 @@ function launchModal() {
 
 let addpetbutton = document.getElementById("addpet");
 addpetbutton.addEventListener("click", launchModal);
+
+let currentAppointmentElement = null;
+
+// used when specific input is clicked and opens modal
+function openModal(el) {
+    currentAppointmentElement = el;
+    document.getElementById('appointmentInput').value = el.innerText;
+    const modal = new bootstrap.Modal(document.getElementById('editModal'));
+    modal.show();
+}
+
+// edits the input field when the modals save button is clicked
+function edit() {
+    const newValue = document.getElementById('appointmentInput').value.trim();
+    
+    if (currentAppointmentElement && newValue) {
+        currentAppointmentElement.innerText = newValue;
+    }
+
+    const modalElement = document.getElementById('editModal');
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
+}
