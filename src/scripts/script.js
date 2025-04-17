14f381eca3cbec44cddf0bb686ed649a11d79927
James H <162525433+JHandlon1@users.noreply.github.com>
2025-04-17 16:33:03 -0400

    I GOT PET THINGS WORKING

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,3 @@
-let currentpet = null;
-
 // Opens the addPet modal
 function launchModal() {
     const modal = new bootstrap.Modal('#add-pet-modal', { keyboard: false });
@@ -38,38 +36,70 @@ function edit() {
     closeModal("editModal");
 }
 
+function createPetButton(name) {
+    let petlist = document.getElementById("petList");
+    let newpet = document.createElement("button");
+    newpet.textContent = name;
+    newpet.classList.add("petNameList");
+    newpet.addEventListener('click', () => setUpDifferentPet(newpet));
+    petlist.appendChild(newpet);
+}
+
+function addPetToLocalStorage(name, breed) {
+    let cur = JSON.parse(localStorage.getItem('pets'));
+    cur.push({
+        'name': name,
+        'breed': breed});
+    localStorage.setItem('pets', JSON.stringify(cur));
+}
+
+function petExists(name) {
+    for (let pet of JSON.parse(localStorage.getItem('pets'))) {
+        if (pet.name == name)
+            return pet;
+    }
+    return null;
+}
+
 // Create new pet and add it to local storage, still needs to add a bunch of info though
 function setUpPetAndLocalStorage() {
     const petName = document.getElementById('petName').value.trim().toLowerCase();
     const petBreed = document.getElementById('petBreed').value.trim().toLowerCase();
     
-    if (localStorage.getItem(petName)) {
+    if (petExists(petName)) {
         alert("Pet with that name already exists, please choose another name or consider making this one more unique :)");
     } else {
-        localStorage.setItem(petName, JSON.stringify({'breed': petBreed}));
+
+        addPetToLocalStorage(petName, petBreed);
         closeModal("add-pet-modal");
-        let petlist = document.getElementById("petList");
-        let newpet = document.createElement("button");
-        newpet.textContent = petName;
-        newpet.classList.add("petNameList");
-        newpet.addEventListener('click', () => setUpDifferentPet(newpet));
-        petlist.appendChild(newpet);
+        createPetButton(petName);
     }
 }
 
 // Change all of the things to do with the new selected pet
 function setUpDifferentPet(pet) {
-    let localpet = JSON.parse(localStorage.getItem(pet.textContent));
-    currentpet = localpet;
-    changeBreedImage();
+    changeBreedImage(pet.textContent);
 }
 
 // Get breed image from API and set the image to it
-function changeBreedImage() {
+function changeBreedImage(petname) {
+    let pet = petExists(petname);
     let image = document.getElementById("actualpicture");
-    fetch ("https://dog.ceo/api/breed/" + currentpet.breed + "/images/random")
+    fetch ("https://dog.ceo/api/breed/" + pet.breed + "/images/random")
         .then(data => data.json())
         .then(da => {
             image.src = da.message;
         });
 }
+
+// Initialization Function
+(function () {
+    if (localStorage.getItem('pets')) {
+        let pets = JSON.parse(localStorage.getItem('pets'));
+        for (let pet of pets) {
+            createPetButton(pet.name);
+        }
+    } else {
+        localStorage.setItem('pets', JSON.stringify([]));
+    }
+})();
