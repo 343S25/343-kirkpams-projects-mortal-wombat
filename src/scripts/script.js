0dd20b13b0609b7065a6a14131a9dfcf5b7d3fe9
James H <162525433+JHandlon1@users.noreply.github.com>
2025-04-17 11:07:28 -0400

    images are finally working omg

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,12 @@
+let currentpet = null;
+
+// Opens the addPet modal
 function launchModal() {
     const modal = new bootstrap.Modal('#add-pet-modal', { keyboard: false });
     modal.show();
 }
 
+// Saw we were reusing code, so this will close a modal with the name passed to it
 function closeModal(elementName) {
     const modalElement = document.getElementById(elementName);
     const modal = bootstrap.Modal.getInstance(modalElement);
@@ -34,14 +38,38 @@ function edit() {
     closeModal("editModal");
 }
 
-function setUpPetLocalStorage() {
+// Create new pet and add it to local storage, still needs to add a bunch of info though
+function setUpPetAndLocalStorage() {
     const petName = document.getElementById('petName').value.trim().toLowerCase();
     const petBreed = document.getElementById('petBreed').value.trim().toLowerCase();
-
+    
     if (localStorage.getItem(petName)) {
         alert("Pet with that name already exists, please choose another name or consider making this one more unique :)");
     } else {
-        localStorage.setItem(petName, {'breed': petBreed});
+        localStorage.setItem(petName, JSON.stringify({'breed': petBreed}));
         closeModal("add-pet-modal");
+        let petlist = document.getElementById("petList");
+        let newpet = document.createElement("button");
+        newpet.textContent = petName;
+        newpet.classList.add("petNameList");
+        newpet.addEventListener('click', () => setUpDifferentPet(newpet));
+        petlist.appendChild(newpet);
     }
+}
+
+// Change all of the things to do with the new selected pet
+function setUpDifferentPet(pet) {
+    let localpet = JSON.parse(localStorage.getItem(pet.textContent));
+    currentpet = localpet;
+    changeBreedImage();
+}
+
+// Get breed image from API and set the image to it
+function changeBreedImage() {
+    let image = document.getElementById("actualpicture");
+    fetch ("https://dog.ceo/api/breed/" + currentpet.breed + "/images/random")
+        .then(data => data.json())
+        .then(da => {
+            image.src = da.message;
+        });
 }
